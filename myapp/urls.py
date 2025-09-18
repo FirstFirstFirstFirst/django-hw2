88c8c601e56aaa49466b99903386b668da5d0bbd
FirstFirstFirstFirst <hocrux1st.first@gmail.com>
2025-09-18 12:49:36 +0700

    add register, profile and editprofile

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from .views import aboutUs, contact, home, showContact
 from django.contrib.auth import views
 from django.shortcuts import render, redirect
 
-from myapp.views import userLogin
+from myapp.views import userLogin, userRegist, userProfile, editProfile
 
 urlpatterns = [
     path("", home, name="home-page"),
@@ -13,5 +13,9 @@ urlpatterns = [
     # path("login/", views.LoginView.as_view(template_name='myapp/login.html'), name='login'),
     path("login/", userLogin, name='login'),
     path("logout/", views.LogoutView.as_view(template_name='myapp/logout.html'), name='logout'),
-    path('showcontact/', showContact, name="showcontact-page")
+    path('showcontact/', showContact, name="showcontact-page"),
+    path('register/', userRegist, name="register-page"),
+    path('profile', userProfile, name="profile-page"),
+    path('editprofile/', editProfile, name="editprofile-page")
+    
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 from .models import *
 
@@ -52,10 +53,78 @@ def userLogin(request):
         except:
             context["message"] = "username or password is incorrect"
 
-    return render(request, "myapp.login.html", context)
+    return render(request, "myapp/login.html", context)
 
 def showContact(request):
     allcontact = contactList.objects.all()
 
     context = {'contact': allcontact}
     return render(request, 'myapp/showcontact.html', context)
+
+def userRegist(request):
+    context = {}
+
+    if request.method == 'POST':
+        data = request.POST.copy()
+        firstname = data.get('firstname')
+        lastname = data.get('lastname')
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        repassword = data.get('repassword')
+
+        try: 
+            User.objects.get(username=username)
+            context['message'] = "Username duplicate"
+        except:
+            newuser = User()
+            newuser.username = username
+            newuser.first_name = firstname
+            newuser.last_name = lastname
+            newuser.email = email
+            
+            if(password == repassword):
+                newuser.set_password(password)
+                newuser.save()
+                newprofile = Profile()
+                newprofile.user = User.objects.get(username=username)
+                newprofile.save()
+                context['message'] = "register complete."
+            else:
+                context['message'] = "password or re-password is incorrect"
+    return render(request, 'myapp/register.html', context)
+
+def userProfile(request):
+    context = {}
+    userprofile = Profile.objects.get(user=request.user)
+    context['profile'] = userprofile
+    return render(request, 'myapp/profile.html', context)
+
+def editProfile(request):
+    
+    context = {}
+    if request.method == 'POST':
+        data = request.POST.copy()
+        firstname = data.get('firstname')
+        lastname = data.get('lastname')
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+
+        current_user = User.objects.get(id=request.user.id)
+        current_user.first_name = firstname
+        current_user.last_name = lastname
+        current_user.username = username
+        current_user.email = email
+        current_user.set_password(password)
+        current_user.save()
+
+        try:
+            user = authenticate(username=current_user.username,
+                    password=password)
+            login(request, user)
+            return redirect('home-page')
+        except:
+            context['message'] = "edit profile fail"
+
+    return render(request, 'myapp/editprofile.html', context)
